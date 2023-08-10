a5b8e9241aca6da2920e51bf14d384c73afbbeed
italorbr <italorosa.profissional@gmail.com>
2023-08-10 20:12:43 -0300

    Initial commit

---
+++ app/src/main/java/br/com/etecia/meuspets/TelaPrincipal_Activity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.meuspets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TelaPrincipal_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tela_principal_layout);
+    }
+}
